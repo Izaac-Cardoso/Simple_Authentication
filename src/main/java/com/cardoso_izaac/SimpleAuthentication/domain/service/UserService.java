a5cf84de381ec92035f5b787715238769f8685f7
Izaac-Cardoso <izaacrcardoso@gmail.com>
2025-08-26 23:24:50 -0300

    Refatoração da lógica de validação dos campos de sinup

--- src/main/java/com/cardoso_izaac/SimpleAuthentication/domain/service/UserService.java
+++ src/main/java/com/cardoso_izaac/SimpleAuthentication/domain/service/UserService.java
@@ -3,19 +3,17 @@ package com.cardoso_izaac.SimpleAuthentication.domain.service;
 import com.cardoso_izaac.SimpleAuthentication.domain.models.User;
 import com.cardoso_izaac.SimpleAuthentication.domain.repositories.UserRepository;
 import com.cardoso_izaac.SimpleAuthentication.dto.SignUpDTO;
-import com.cardoso_izaac.SimpleAuthentication.dto.SignInDTO;
+import com.cardoso_izaac.SimpleAuthentication.exceptions.BadCredentialsException;
 import com.cardoso_izaac.SimpleAuthentication.exceptions.DuplicatedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+import java.util.function.Predicate;
 
 @Service
 public class UserService {
@@ -32,11 +30,22 @@ public class UserService {
 
     @Transactional
     public ResponseEntity<?> createUser(SignUpDTO request) {
-        if(request.name() == null || request.name().isBlank()
-                || request.password() == null || request.password().isBlank()
-                || request.email() == null || request.email().isBlank()) {
-            throw new BadCredentialsException("One or more invalid fields.");
-        }
+
+        Predicate<String> notValid = (credential) -> credential == null || credential.isBlank();
+
+        // Map the fields to their respective values passed in request
+        Map<String, String> fields = Map.of(
+                "name", request.name(),
+                "password", request.password(),
+                "email", request.email());
+
+        // check if there's an invalid field
+        fields.entrySet().stream()
+                .filter(input -> notValid.test(input.getValue()))
+                .findFirst()
+                .ifPresent(input -> {
+                    throw new BadCredentialsException("Invalid field: " + input.getKey());
+                });
 
         var email = request.email();
         var currentUser = repository.findByEmail(email);
@@ -51,16 +60,4 @@ public class UserService {
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
-//    public String login(SignInDTO request) {
-//        Authentication auth;
-//        try{ auth = authManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(request.name(), request.password()));
-//
-//            SecurityContextHolder.getContext().setAuthentication(auth);
-//
-//        } catch(BadCredentialsException e) {
-//            e.getMessage();
-//        }
-//    }
 }
