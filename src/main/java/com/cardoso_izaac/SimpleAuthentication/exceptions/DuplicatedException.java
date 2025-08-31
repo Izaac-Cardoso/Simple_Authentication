5996a0e3673f1e735bf8a452bd38801e55761c61
Izaac-Cardoso <izaacrcardoso@gmail.com>
2025-08-31 13:47:54 -0300

    Refatoração do global exceptionHandler

--- src/main/java/com/cardoso_izaac/SimpleAuthentication/exceptions/DuplicatedException.java
+++ src/main/java/com/cardoso_izaac/SimpleAuthentication/exceptions/DuplicatedException.java
@@ -1,5 +1,9 @@
 package com.cardoso_izaac.SimpleAuthentication.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicatedException extends RuntimeException {
 
    public DuplicatedException(String message) {
