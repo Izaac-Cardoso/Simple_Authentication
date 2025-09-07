1cd730469910357fa65aa4ddaae74b5cc0eaa75d
Izaac-Cardoso <izaacrcardoso@gmail.com>
2025-09-07 17:31:41 -0300

    Incluindo Classe BadCredentialsException

---
+++ src/main/java/com/cardoso_izaac/SimpleAuthentication/exceptions/BadCredentialsException.java
@@ -0,0 +1,12 @@
+package com.cardoso_izaac.SimpleAuthentication.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadCredentialsException extends RuntimeException {
+
+    public BadCredentialsException(String massage) {
+        super(massage);
+    }
+}
